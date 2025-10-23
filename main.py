52bd6b6999709082ee43b0727d2348b2ca4a4bdb
sreyash <sreyash.s@outlook.com>
2025-10-23 15:47:59 +0530

    initial station class

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
+import csv
+
 
 class Station:
+
+    stations: dict = {}
+
     def __init__(self, id: int):
-        pass
+        self.id = id
+
+    @property
+    def name(self):
+        with open("stations.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if int(row["id"]) == self.id:
+                    return row["name"]
+                
+    @classmethod
+    def load_stations(cls):
+        with open("stations.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls.stations[row["id"]] = Station(int(row["id"]))
+
+    @classmethod
+    def display_stations(cls):
+        for station in cls.stations:
+            print(f'Station Name: {cls.stations[station].name}\nStation ID: {station}\n')
+
 
 class Line:
+
+    lines: list = []
+
     def __init__(self):
         pass
 
+
 class Ticket:
-    def __init__(self):
-        pass  
+
+    tickets = []
+
+    def __init__(self, id: int):
+        self.id = id
+
+    @property
+    def start_id(self):
+        pass
