341809b771d1c330d194e170d2129c31e832d1ce
sreyash <sreyash.s@outlook.com>
2025-10-23 18:47:33 +0530

    basic version of the Line class

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import csv
 
 
 class Station:
-
+    
     stations: dict = {}
 
     def __init__(self, id: int):
         self.id = id
+
+    def __repr__(self) -> str:
+        return str(self.name)
 
     @property
     def name(self):
@@ -16,25 +19,53 @@ class Station:
                 if int(row["id"]) == self.id:
                     return row["name"]
                 
-    @classmethod
-    def load_stations(cls):
+    @property
+    def neighbours(self):
         with open("stations.csv", "r") as file:
             reader = csv.DictReader(file)
             for row in reader:
-                cls.stations[row["id"]] = Station(int(row["id"]))
+                if int(row["id"]) == self.id:
+                    return row["neighbours"].split("$")
+                
+    @classmethod
+    def load(cls):
+        with open("stations.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls.stations[int(row["id"])] = Station(int(row["id"]))
 
     @classmethod
-    def display_stations(cls):
+    def display(cls):
         for station in cls.stations:
-            print(f'Station Name: {cls.stations[station].name}\nStation ID: {station}\n')
+            print(f'[{station}] => {cls.stations[station].name}')
 
 
 class Line:
 
     lines: list = []
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+                
+    @property
+    def stations(self) -> list:
+        with open("lines.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["name"] == self.name:
+                    return row["stations"].split("$")
+            else:
+                return []
+
+    @classmethod
+    def load(cls):
+        with open("lines.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls.lines.append(Line(row["name"]))
 
 
 class Ticket:
